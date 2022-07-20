9f363191b91846db8c4de70d999014b7664eca80
Faizan Ahmad <faizan13801@gmail.com>
2022-07-20 21:09:42 +0530

    ProGrad ID

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//Enter your code here..
+async function contactList(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data);
+        var player='<h2>Lists of Users</h2>';
+                    data.forEach(function(user) {
+                     player+=
+                    `<div class="player">
+                      <div class="strength">Name : ${user.name}</div>
+                      <div>Email   : ${user.email}</div>
+                      <div>Phone   : ${user.phone}</div>
+                      <div>Website : ${user.website}</div>
+                      <div>Company : ${user.company.name}</div>
+                      <div>City    : ${user.address.city}</div>
+                      <div>Zipcode : ${user.address.zipcode}</div>
+                     </div>`
+                    });
+        let ele = document.getElementById('message');
+        ele.innerHTML = player;
+    }
+    catch(error){
+        console.log('Promise rejected.');
+        console.log(error.message);
+    }
+}
